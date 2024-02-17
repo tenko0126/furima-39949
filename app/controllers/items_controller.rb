f5d473f0b2b2c9ca1242cd212cce5fd28ef46e14
tenko0126 <kato_yuu@msn.com>
2024-02-17 20:53:01 +0900

    商品削除機能

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,6 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, except: [:index, :show]
-  before_action :set_item, only:[:show,:edit, :update]
+  before_action :set_item, only:[:show,:edit, :update, :destroy]
   before_action :move_to_index, only: [:edit, :update]
 
   def index
@@ -32,6 +32,13 @@ class ItemsController < ApplicationController
     else
       render :edit, status: :unprocessable_entity
     end
+  end
+
+  def destroy
+    if current_user.id == @item.user_id
+      @item.destroy
+    end
+    redirect_to root_path
   end
 
   private
